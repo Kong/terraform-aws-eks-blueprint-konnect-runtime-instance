ed4e54e6d5b6bb91c496cd25a3f1c92d65f86a81
Nadeem Rahaman <rahanade@amazon.com>
2023-06-18 12:16:20 +0530

    made tags as variable in kong_helm module

--- variables.tf
+++ variables.tf
@@ -47,10 +47,3 @@ variable "create_delay_dependencies" {
   type        = list(string)
   default     = []
 }
-
-
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
