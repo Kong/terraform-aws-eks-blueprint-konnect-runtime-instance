2b818e1927164ef2b45b43895cb0f992fcc719db
Anuj Sharma <anshrma@amazon.com>
2023-07-07 09:48:05 -0700

    irsa removal and related module changes

--- main.tf
+++ main.tf
@@ -19,51 +19,48 @@ resource "kubernetes_namespace_v1" "kong" {
   }
 }
 
-###########Service Account###########
+###########AddOns for Cluster. Note the module name has addon"s"###########
 
-resource "kubernetes_service_account_v1" "kong" {
-  count = var.enable_kong_konnect && local.create_kubernetes_service_account ? 1 : 0
+module "add_ons" {
+  source = "aws-ia/eks-blueprints-addons/aws"
+  version = "1.1.0"
+
+  cluster_name      = var.cluster_name
+  cluster_endpoint  = var.cluster_endpoint
+  cluster_version   = var.cluster_version
+  oidc_provider_arn = var.oidc_provider_arn
+
+  # EKS Add-on
+  eks_addons = {
+    coredns    = {}
+    vpc-cni    = {}
+    kube-proxy = {}
+  }
+
+  enable_external_secrets = true
+  // Following to ensure that the IRSA with which the External Secret Pod is running does not have any access. 
+  // Ideally, this should not use IRSA at all as its the property of `SecretStore` CRD
+  external_secrets_secrets_manager_arns = []
+  external_secrets_ssm_parameter_arns = []
+}
+
+
+##########Service Account for External Secret###########
+
+resource "kubernetes_service_account_v1" "external_secret_sa" {
   metadata {
-    name        = local.service_account
-
-    namespace   = try(kubernetes_namespace_v1.kong[0].metadata[0].name, local.namespace)
-    annotations = { "eks.amazonaws.com/role-arn" : module.kong_irsa[0].iam_role_arn }
+    name        = local.external_secret_service_account_name
+    namespace   = local.namespace
+    annotations = { "eks.amazonaws.com/role-arn" : module.external_secret_irsa[0].iam_role_arn }
   }
 
   automount_service_account_token = true
 }
 
-###########Kong Helm Module##########
+## IRSA FOR EXTERNAL SECRET STORE OBJECT ##
+# Note, this source module does not has "s" in eks-blueprints-addon
 
-module "kong_helm" {
-  source           = "aws-ia/eks-blueprints-addon/aws"
-  version          = "1.1.0"
-
-  create           = var.enable_kong_konnect
-  chart            = local.name
-  chart_version    = local.chart_version
-  repository       = local.repository
-  description      = "Kong konnect"
-  namespace        = local.namespace
-  create_namespace = false
-
-  set              = local.set_values
-  values           = local.values
-
-  set_irsa_names = ["deployment.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"]
-  # # Equivalent to the following but the ARN is only known internally to the module
-  # set = [{
-  #   name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-  #   value = iam_role_arn.this[0].arn
-  # }]
-
-  tags = var.tags
-  depends_on = [kubectl_manifest.secret]
-}
-
-###########IRSA###########
-
-module "kong_irsa" {
+module "external_secret_irsa" {
   count   = var.enable_kong_konnect ? 1 : 0
   source  = "aws-ia/eks-blueprints-addon/aws"
   version = "1.1.0"
@@ -71,19 +68,19 @@ module "kong_irsa" {
   create_release = false
   # IAM role for service account (IRSA)
   create_role                   = true
-  role_name                     = local.role_name
-  role_name_use_prefix          = try(var.kong_config.role_name_use_prefix, true)
-  role_path                     = try(var.kong_config.role_path, "/")
-  role_permissions_boundary_arn = lookup(var.kong_config, "role_permissions_boundary_arn", null)
-  role_description              = try(var.kong_config.role_description, "IRSA for kong")
-  role_policies                 = lookup(var.kong_config, "role_policies", {})
+  role_name                     = local.external_secrets_irsa_role_name
+  role_name_use_prefix          = local.external_secrets_irsa_role_name_use_prefix
+  role_path                     = local.external_secrets_irsa_role_path
+  role_permissions_boundary_arn = local.external_secrets_irsa_role_permissions_boundary_arn
+  role_description              = local.external_secrets_irsa_role_description
+  role_policies                 = local.external_secrets_irsa_role_policies
   source_policy_documents = compact(concat(
-    data.aws_iam_policy_document.kong_secretstore[*].json,
+    data.aws_iam_policy_document.kong_external_secret_secretstore[*].json,
     lookup(var.kong_config, "source_policy_documents", [])
   ))
   override_policy_documents = lookup(var.kong_config, "override_policy_documents", [])
   policy_statements         = lookup(var.kong_config, "policy_statements", [])
-  policy_name               = try(var.kong_config.policy_name, "kong")
+  policy_name               = try(var.kong_config.policy_name, "external-secrets-irsa-policy")
   policy_name_use_prefix    = try(var.kong_config.policy_name_use_prefix, true)
   policy_path               = try(var.kong_config.policy_path, null)
   policy_description        = try(var.kong_config.policy_description, "IAM Policy for Kong")
@@ -93,7 +90,7 @@ module "kong_irsa" {
       provider_arn = var.oidc_provider_arn
       # namespace is inherited from chart
       namespace       = local.namespace
-      service_account = local.service_account
+      service_account = local.external_secret_service_account_name
     }
   }
 }
@@ -116,11 +113,12 @@ spec:
       auth:
         jwt:
           serviceAccountRef:
-            name: ${local.service_account}
+            name: ${local.external_secret_service_account_name}
 YAML
   depends_on = [
-    module.kong_irsa,
-    kubernetes_service_account_v1.kong
+    module.external_secret_irsa,
+    kubernetes_service_account_v1.external_secret_sa,
+    module.add_ons // Dont remove this dependency
   ]
 }
 
@@ -154,3 +152,38 @@ spec:
 YAML
   depends_on = [kubectl_manifest.secretstore]
 }
+
+###########Kong Helm Module##########
+# Note, this source module does not has "s" in eks-blueprints-addon
+
+module "kong_helm" {
+  source           = "aws-ia/eks-blueprints-addon/aws"
+  version          = "1.1.0"
+
+  create           = var.enable_kong_konnect
+  chart            = local.name
+  chart_version    = local.chart_version
+  repository       = local.repository
+  description      = "Kong konnect"
+  namespace        = local.namespace
+  create_namespace = false
+
+  set              = local.set_values
+  values           = local.values
+
+  tags = var.tags
+  depends_on = [
+    module.add_ons,
+    kubectl_manifest.secret
+  ]
+
+}
+
+## Not required, but commenting, so that in future, if we need to retrieve the ARN of the secret, thats doable by data.aws_secretsmanager_secret.cert_secret_name.arn
+# data "aws_secretsmanager_secret" "cert_secret_name" {
+#   name = local.cert_secret_name
+# }
+
+# data "aws_secretsmanager_secret" "key_secret_name" {
+#   name = local.key_secret_name
+# }
